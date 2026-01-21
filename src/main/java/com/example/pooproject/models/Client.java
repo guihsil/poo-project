66eadbe8f3d1c809e0a0c1d5f8bdd2f46fa51731
guihsil <guilherme1234oliveira.go@gmail.com>
2026-01-21 15:18:12 -0300

    nomeia o commit ai

---
+++ src/main/java/com/example/pooproject/models/Client.java
@@ -0,0 +1,5 @@
+package com.example.pooproject.models;
+
+public class Client extends User{
+
+}
---
+++ src/main/java/com/example/pooproject/models/User.java
@@ -0,0 +1,8 @@
+package com.example.pooproject.models;
+
+public abstract class User {
+    String name;
+    int age;
+    String email;
+    String password;
+}
